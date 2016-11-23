d7570c85bb89404b192532f0d411f0a4cad3c54f
DaanDirker <Daan.dirker@hva.nl>
2016-11-23 22:30:20 +0100

    "Start/open" paneel tabel bevat database informatie

---
+++ src/testswitch/Bagage.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testswitch;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * @author Daan Dirker
+ */
+public class Bagage {
+    
+    private final SimpleIntegerProperty id = new SimpleIntegerProperty();
+    private final SimpleIntegerProperty datum = new SimpleIntegerProperty();
+    private final SimpleIntegerProperty labelNummer = new SimpleIntegerProperty();
+    private final SimpleIntegerProperty vluchtNr = new SimpleIntegerProperty();
+    private final SimpleStringProperty bagageType = new SimpleStringProperty();
+
+    //Setters
+    public void setId(int id){
+        this.id.set(id);
+    }
+    public void setDatum(int datum){
+        this.datum.set(datum);
+    }
+    public void setLabelNummer(int labelNummer){
+        this.labelNummer.set(labelNummer);
+    }
+    public void setVluchtNr(int vluchtNr){
+        this.vluchtNr.set(vluchtNr);
+    }
+    public void setBagageType(String bagageType){
+        this.bagageType.set(bagageType);
+    }
+    
+    //Getters
+    public Integer getId(){
+        return id.get();
+    }
+    public Integer getDatum(){
+        return datum.get();
+    }
+    public Integer getLabelNummer(){
+        return labelNummer.get();
+    }
+    public Integer getVluchtNr(){
+        return vluchtNr.get();
+    }
+    public String getBagageType(){
+        return bagageType.get();
+    }
+    
+}
---
+++ src/testswitch/StartController.java
@@ -0,0 +1,109 @@
+package testswitch;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * @author Daan Dirker
+ */
+public class StartController implements Initializable {
+
+    //Gevonden bagage
+    @FXML public TableView<Bagage> gevondenTabel;
+    @FXML public TableColumn<Bagage, Integer> gevondenIdKolom;
+    @FXML public TableColumn<Bagage, Integer> gevondenDatumKolom;
+    @FXML public TableColumn<Bagage, Integer> gevondenLabelNummerKolom;
+    @FXML public TableColumn<Bagage, Integer> gevondenVluchtNrKolom;
+    @FXML public TableColumn<Bagage, String> gevondenBagageTypeKolom;
+    
+    //Vermiste bagage
+    @FXML public TableView<Bagage> vermisteTabel;
+    @FXML public TableColumn<Bagage, Integer> vermisteIdKolom;
+    @FXML public TableColumn<Bagage, Integer> vermisteDatumKolom;
+    @FXML public TableColumn<Bagage, Integer> vermisteLabelNummerKolom;
+    @FXML public TableColumn<Bagage, Integer> vermisteVluchtNrKolom;
+    @FXML public TableColumn<Bagage, String> vermisteBagageTypeKolom;
+    
+    private ObservableList<Bagage> gevondenData = FXCollections.observableArrayList();
+    private ObservableList<Bagage> vermisteData = FXCollections.observableArrayList();
+
+    @FXML
+    private void writeTableData(String tabelNaam) {
+        Database database = new Database(
+            "testDatabase",
+            "ronpelt.synology.me:3306",
+            "root",
+            "kGjMtEO06BPiu2u4"
+        );
+
+        try {
+            ResultSet result = database.executeQuery("SELECT * FROM testDatabase." 
+                    + tabelNaam + ";");
+
+            //Gaat net zo lang door, tot er geen records meer zijn
+            while (result.next()) {
+                Bagage bagage = new Bagage();
+                bagage.setId(result.getInt("id" + tabelNaam));
+                bagage.setDatum(result.getInt("Datum"));
+                bagage.setLabelNummer(result.getInt("Labelnummer"));
+                bagage.setVluchtNr(result.getInt("Vluchtnummer"));
+                bagage.setBagageType(result.getString("BagageType"));  
+                
+                if (tabelNaam == "Gevonden") {
+                    gevondenData.add(bagage);
+                } else if (tabelNaam == "Vermist") {
+                    vermisteData.add(bagage);
+                }
+                
+            }
+
+        } catch (SQLException ex) {
+
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Kollomen worden gelinkt aan Atributen van de Person class
+        gevondenIdKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("id"));
+        gevondenDatumKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("datum"));
+        gevondenLabelNummerKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("labelNummer"));
+        gevondenVluchtNrKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("vluchtNr"));
+        gevondenBagageTypeKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, String>("bagageType"));
+        
+        //Vermist
+        vermisteIdKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("id"));
+        vermisteDatumKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("datum"));
+        vermisteLabelNummerKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("labelNummer"));
+        vermisteVluchtNrKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, Integer>("vluchtNr"));
+        vermisteBagageTypeKolom.setCellValueFactory(
+                new PropertyValueFactory<Bagage, String>("bagageType"));
+                        
+        gevondenTabel.setItems(gevondenData);
+        vermisteTabel.setItems(vermisteData);
+        writeTableData("Gevonden");
+        writeTableData("Vermist");
+    }
+
+}
