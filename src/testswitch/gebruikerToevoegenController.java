9a0a278b6f04a9dd38bd60ee952ac017ac71329b
MaartenMes94 <Maarten.mes@hva.nl>
2016-11-25 15:17:28 +0100

    Begin van gebruikerToevoegenController, werkt nog niet volledig.

--- src/testswitch/gebruikerToevoegenController.java
+++ src/testswitch/gebruikerToevoegenController.java
@@ -26,7 +26,7 @@ public class gebruikerToevoegenController {
     private TextField FXVoornaam, FXTussenvoegsel, FXAchternaam, FXGebruikersnaam;
     @FXML
     private TextField FXWachtwoord, FXEmail, FXTelefoonnummer;
-
+    
     //Boolean checkbox positie
     @FXML
     private CheckBox ManagerPosition;
@@ -41,23 +41,32 @@ public class gebruikerToevoegenController {
     Database database = new Database(DB_NAME, DB_SERVER, DB_ACCOUNT, DB_PASSWORD);
 
     public void handle(ActionEvent event) throws SQLException {
-        String query = "INSERT INTO testDatabase.Gebruikers (Voornaam) VALUES (?);";
+        String query = "INSERT INTO testDatabase.Gebruikers"
+                + " (Voornaam, Tussenvoegsel, Achternaam,"
+                + " Username, Password, Telefoonnummer, Email)"
+                + " VALUES (?,?,?,?,?,?,?);";
         PreparedStatement statement = database.prepareStatement(query);
         try {
-            if (FXVoornaam.getText().equals("")) {
-                System.out.println("error");
-                return;
-            }
             statement.setString(1, FXVoornaam.getText());
+            statement.setString(2, FXTussenvoegsel.getText());
+            statement.setString(3, FXAchternaam.getText());            
+            statement.setString(4, FXGebruikersnaam.getText());
+            statement.setString(5, FXWachtwoord.getText());
+            statement.setString(6, FXTelefoonnummer.getText());
+            statement.setString(7, FXEmail.getText());
+            
             statement.executeUpdate();
+            
+            
         } catch (Exception e) {
-            // log info somewhere at least until it's properly tested/
-            // you implement a better way of handling the error
-            e.printStackTrace(System.err);
+             // log info somewhere at least until it's properly tested/
+             // you implement a better way of handling the error
+             e.printStackTrace(System.err);
         }
-        MainNavigator.loadVista(MainNavigator.GEBRUIKER);
     }
-
+    
+    
+    
     @FXML
     private void gebruikerToevoegenCancel(ActionEvent event) throws IOException {
         MainNavigator.loadVista(MainNavigator.GEBRUIKER);
