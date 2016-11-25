cda9be5cbc2a5f88f2d87070183a776460d99ac3
peltos <pelt8@hotmail.com>
2016-11-25 15:22:37 +0100

    error als gebruik fields leeg zijn

--- src/testswitch/gebruikerToevoegenController.java
+++ src/testswitch/gebruikerToevoegenController.java
@@ -26,7 +26,7 @@ public class gebruikerToevoegenController {
     private TextField FXVoornaam, FXTussenvoegsel, FXAchternaam, FXGebruikersnaam;
     @FXML
     private TextField FXWachtwoord, FXEmail, FXTelefoonnummer;
-    
+
     //Boolean checkbox positie
     @FXML
     private CheckBox ManagerPosition;
@@ -47,26 +47,31 @@ public class gebruikerToevoegenController {
                 + " VALUES (?,?,?,?,?,?,?);";
         PreparedStatement statement = database.prepareStatement(query);
         try {
+            if (FXVoornaam.getText().equals("") || FXTussenvoegsel.getText().equals("")
+                    || FXAchternaam.getText().equals("") || FXGebruikersnaam.getText().equals("")
+                    || FXWachtwoord.getText().equals("")) 
+            {
+                System.out.println("error");
+                return;
+            }
             statement.setString(1, FXVoornaam.getText());
             statement.setString(2, FXTussenvoegsel.getText());
-            statement.setString(3, FXAchternaam.getText());            
+            statement.setString(3, FXAchternaam.getText());
             statement.setString(4, FXGebruikersnaam.getText());
             statement.setString(5, FXWachtwoord.getText());
             statement.setString(6, FXTelefoonnummer.getText());
             statement.setString(7, FXEmail.getText());
-            
+
             statement.executeUpdate();
-            
-            
+
         } catch (Exception e) {
-             // log info somewhere at least until it's properly tested/
-             // you implement a better way of handling the error
-             e.printStackTrace(System.err);
+            // log info somewhere at least until it's properly tested/
+            // you implement a better way of handling the error
+            e.printStackTrace(System.err);
         }
+        MainNavigator.loadVista(MainNavigator.GEBRUIKER);
     }
-    
-    
-    
+
     @FXML
     private void gebruikerToevoegenCancel(ActionEvent event) throws IOException {
         MainNavigator.loadVista(MainNavigator.GEBRUIKER);
