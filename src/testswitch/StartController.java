168af426aa88e3e067c37db92b9bfc18a4f1c1fe
peltos <pelt8@hotmail.com>
2016-11-28 16:53:46 +0100

    gegevens life lezen op start pagina

--- src/testswitch/StartController.java
+++ src/testswitch/StartController.java
@@ -9,10 +9,11 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
 /**
  * @author Daan Dirker
@@ -73,6 +74,78 @@ public class StartController implements Initializable {
         }
 
     }
+    
+    @FXML public Label gevondenDatum;
+    @FXML public Label gevondenTijd;
+    @FXML public Label gevondenLuchthaven;
+    @FXML public Label gevondenID;
+    @FXML public Label gevondenType;
+    @FXML public Label gevondenMerk;
+    @FXML public Label gevondenKleur;
+    @FXML public Label gevondenBK;
+    @FXML public Label gevondenLabelNr;
+    @FXML public Label gevondenVluchtNr;
+    @FXML public Label gevondenBestemming;
+
+    @FXML
+    private void gevondenSelected() {
+        
+        Bagage bagage = gevondenTabel.getSelectionModel().getSelectedItem();
+        gevondenDatum.setText(bagage.getDatum());
+        gevondenTijd.setText(bagage.getTijd());
+        gevondenLuchthaven.setText(bagage.getLuchthaven());
+        gevondenID.setText(String.valueOf(bagage.getId()));
+        gevondenType.setText(bagage.getBagageType());
+        gevondenMerk.setText(bagage.getMerk());
+        gevondenKleur.setText(bagage.getKleur());
+        gevondenBK.setText(bagage.getBijzondereKenmerken());
+        gevondenLabelNr.setText(String.valueOf(bagage.getLabelNummer()));
+        gevondenVluchtNr.setText(String.valueOf(bagage.getVluchtNr()));
+        gevondenBestemming.setText(bagage.getBestemming());
+    } 
+    
+    @FXML public Label vermisteDatum;
+    @FXML public Label vermisteTijd;
+    @FXML public Label vermisteLuchthaven;
+    @FXML public Label vermisteID;
+    @FXML public Label vermisteNaam;
+    @FXML public Label vermisteAdres;
+    @FXML public Label vermisteWoonplaats;
+    @FXML public Label vermistePostcode;
+    @FXML public Label vermisteLand;
+    @FXML public Label vermisteTelefoon;
+    @FXML public Label vermisteEmail;
+    @FXML public Label vermisteType;
+    @FXML public Label vermisteMerk;
+    @FXML public Label vermisteKleur;
+    @FXML public Label vermisteBK;
+    @FXML public Label vermisteLabelNr;
+    @FXML public Label vermisteVluchtNr;
+    @FXML public Label vermisteBestemming;
+    
+    @FXML
+    private void vermisteSelected() {
+        
+        Bagage bagage = vermisteTabel.getSelectionModel().getSelectedItem();
+        vermisteDatum.setText(bagage.getDatum());
+        vermisteTijd.setText(bagage.getTijd());
+        vermisteLuchthaven.setText(bagage.getLuchthaven());
+        vermisteID.setText(String.valueOf(bagage.getId()));
+        vermisteNaam.setText(bagage.getNaam());
+        vermisteAdres.setText(bagage.getAdres());
+        vermisteWoonplaats.setText(bagage.getWoonplaats());
+//        vermistePostcode.setText(bagage.get);
+        vermisteLand.setText(bagage.getLand());
+        vermisteTelefoon.setText(String.valueOf(bagage.getTelefoonnummer()));
+        vermisteEmail.setText(bagage.getEmail());
+        vermisteType.setText(bagage.getBagageType());
+        vermisteMerk.setText(bagage.getMerk());
+        vermisteKleur.setText(bagage.getKleur());
+        vermisteBK.setText(bagage.getBijzondereKenmerken());
+        vermisteLabelNr.setText(String.valueOf(bagage.getLabelNummer()));
+        vermisteVluchtNr.setText(String.valueOf(bagage.getVluchtNr()));
+        vermisteBestemming.setText(bagage.getBestemming());
+    } 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
